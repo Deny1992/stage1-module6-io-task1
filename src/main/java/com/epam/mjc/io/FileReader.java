d3f87262f3428680da6360470b635ea3cff3c91e
Denyonepeny <denisshulga1992@gmail.com>
2023-01-09 21:13:27 +0300

    done

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,33 @@
 package com.epam.mjc.io;
 
-import java.io.File;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        List<String> result = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file)))) {
+            while (reader.ready()){
+                String[] strs = reader.readLine().split(" ");
+                result.add(strs[1]);
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        String name = result.get(0);
+        Integer age = Integer.parseInt(result.get(1));
+        String email = result.get(2);
+        Long phone = Long.parseLong(result.get(3));
+
+        Profile profile = new Profile(name, age, email, phone);
+
+        return  profile;
     }
 }
